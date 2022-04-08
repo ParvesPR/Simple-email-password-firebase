85766c142cdc3153b3fead64db816ac63c94e170
ParvesPR <parves2585@gmail.com>
2022-04-08 11:40:39 +0800

    linked with firebase

--- src/App.js
+++ src/App.js
@@ -1,47 +1,54 @@
-import { getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import app from "./firebase.init";
+import { useState } from "react";
 
 const auth = getAuth(app);
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const handleEmailBlur = event => {
-    console.log(event.target.value)
+    setEmail(event.target.value)
   }
   const handlePasswordBlur = event => {
-    console.log(event.target.value)
+    setPassword(event.target.value)
   }
   const handleSubmitBtn = event => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(result => {
+        const user = result.user;
+        console.log(user)
+      })
+      .catch()
     event.preventDefault();
   }
   return (
     <div>
 
-   <div className="registration w-50 mx-auto">
-     <h1 className="text-center">Please Resister</h1>
-   <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
+      <div className="registration w-50 mx-auto">
+        <h1 className="text-center">Please Resister</h1>
+        <Form onSubmit={handleSubmitBtn}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-   </div>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
 
     </div>
   );
